c6e2dfed53145acee1856a3858d295b32358d889
jason.brown <jason.brown@checkr.com>
2025-10-26 14:44:19 -0600

    boat is abstract class

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,15 @@
 class Boat < ApplicationRecord
   belongs_to :seller, class_name: "User", inverse_of: :boats, optional: true
+
+  class << self
+    def new(*args, **kwargs, &block)
+      return super unless self == Boat
+      raise NotImplementedError, "Boat is abstract; instantiate a subclass (e.g., Melges15, ILCA)"
+    end
+  end
+
+  def initialize(*args, **kwargs)
+    raise NotImplementedError, "Boat is abstract; instantiate a subclass" if self.class == Boat
+    super
+  end
 end
